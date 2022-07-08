90fa4678fa605ef44bc686237b224b86723da520
Duane Cilliers <hello@duane.co.za>
2022-07-09 00:24:33 +0200

    add homepage hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,41 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Navbar from '../components/navbar'
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Duane Cilliers</title>
-        <meta name="description" content="Frontend Developer" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <div className="hero min-h-screen">
+          <div className="hero-content">
+            <div className="max-w-2xl">
+              <h1 className="text-6xl font-bold font-primary">
+                <span className="text-2xl mb-2 block">Duane Cilliers</span>
+                Frontend Developer Based in Cape Town.
+              </h1>
+              <p className="py-6 leading-7">
+                Currently working at{' '}
+                <a className="link" href="https://reos.co.za/" target="_blank" rel="noreferrer">
+                  reOS
+                </a>
+                &nbsp;and&nbsp;
+                <a className="link" href="https://trustprotects.me/" target="_blank" rel="noreferrer">
+                  Trust My Travel
+                </a>
+                . Exploring advanced TypeScript, creative development and toying with the latest tech.
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
