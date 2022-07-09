204be7df7951b76398e01fbffebc27f09e88dd77
Duane Cilliers <hello@duane.co.za>
2022-07-09 16:29:03 +0200

    fix tailwind dark mode class name

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  darkMode: ['class', '[data-theme="dark"]'],
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
