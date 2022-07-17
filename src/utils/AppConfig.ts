598745bf2593b80f89df48069a7e6dc0f2d0cfb5
Duane Cilliers <hello@duane.co.za>
2022-07-17 17:42:01 +0200

    replace next setup with astro

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+  site_name: 'Duane Cilliers',
+  title: 'Duane Cilliers',
+  description: 'Frontend Developer Based in Cape Town',
+  author: 'Duane',
+  locale_region: 'en-za',
+  locale: 'za',
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,14 @@
-const colors = require('tailwindcss/colors')
-/** @type {import('tailwindcss').Config} */
-
+/* eslint-disable import/no-extraneous-dependencies, global-require */
 module.exports = {
-  darkMode: ['class', '[data-theme="dark"]'],
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}',
+    './node_modules/astro-boilerplate-components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
-  daisyui: {
-    styled: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: '',
-    themes: [
-      {
-        light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=winter]'],
-          'base-content': colors.sky[800],
-        },
-        dark: {
-          ...require('daisyui/src/colors/themes')['[data-theme=luxury]'],
-          'base-content': colors.amber[500],
-        },
-      },
-    ],
-  },
-}
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
+};
