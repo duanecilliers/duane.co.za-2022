4cf81e2dffef41335bd60e9f3d3acd9624c0df55
Duane Cilliers <hello@duane.co.za>
2022-07-18 10:05:25 +0200

    refactor contributions canvas to prevent shift of layout

---
+++ src/components/ContributionsCanvas.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+
+import { drawContributions } from '@/modules/github/contributions';
+
+type Props = {
+  contributions: ContributionsData;
+};
+
+const ContributionsCanvas = ({ contributions }: Props) => {
+  useEffect(() => {
+    const contributionsCanvas = document.getElementById(
+      'contributions-canvas'
+    ) as HTMLCanvasElement;
+    if (contributionsCanvas && contributions) {
+      drawContributions(
+        contributionsCanvas,
+        contributions.data.user.contributionsCollection,
+        'dark'
+      );
+    }
+  });
+
+  return (
+    <canvas
+      id="contributions-canvas"
+      width={200}
+      height={200}
+      // className="lg:block w-screen h-screen absolute opacity-10 top-0 left-0 z-0 pt-48"
+      className="lg:block w-screen h-screen absolute top-0 left-0 z-0 pt-48"
+    />
+  );
+};
+
+export default ContributionsCanvas;
